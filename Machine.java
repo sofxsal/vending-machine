d28a569518986920b2f6852721daf40a5df1401a
SOFIA NGAPANI <withsofina@gmail.com>
2022-12-02 16:36:50 +0800

    constructor, get and set method done

--- Machine.java
+++ Machine.java
@@ -1,10 +1,30 @@
 public class Machine {
     
-    // 2.1 add fields
-    private String[][] name;
-    private double[][] price;
-    private int[][] quantity;
+    // 1. add fields
+    // items are arranged in rows, hence 2d arrays
+    private Item[][] items;
 
+    // 2. The Big 3 - Constructor
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        
+        for(int i = 0; i < items.length; i++) {
+            for(int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+            
+        }
+    }
+
+    // 2. The Big 3 - get method
+    public Item getItem(int row, int spot) {
+        return new Item(items[row][spot]);
+    }
+
+    // 3. The Big 3 - set method
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
 
     /**
      * Function name – dispense
