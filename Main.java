1aa40641ab5c8de5e8799f12115416d58d12298c
SOFIA NGAPANI <withsofina@gmail.com>
2022-12-04 17:54:53 +0800

    part 4

--- Main.java
+++ Main.java
@@ -6,15 +6,7 @@ public class Main {
         System.out.println("\t************************************************");
         System.out.println("\t             WELCOME TO JAVA DRINKS!            ");
         System.out.println("\t************************************************");
-        
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Pick a row: ");
-        int row = scan.nextInt();
-
-        System.out.println("Pick a spot in the row: ");
-        int spot = scan.nextInt();
-
+    
         Item[][] items = new Item[][] {
             { new Item("Pepsi", 1.99, 3) , new Item(" Fresca", 1.49, 3), new Item(" Brisk", 2.49, 2) },
             { new Item("Fanta", 1.99, 2) , new Item(" Barq's", 1.49, 2), new Item(" A & W", 2.49, 3) },
@@ -25,6 +17,15 @@ public class Main {
         // 2.3. create machine object of Machine class
         Machine machine = new Machine(items);
 
+        // 4. create scanner
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Pick a row: ");
+        int row = scan.nextInt();
+
+        System.out.println("Pick a spot in the row: ");
+        int spot = scan.nextInt();
+        
         machine.dispense(row, spot);
         System.out.println("\n" + machine);
        
